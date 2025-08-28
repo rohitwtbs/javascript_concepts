2959dd026eb1627530e211205acbdbf2509e7a15
rohit kumar <12rohit4@gmail.com>
2025-08-28 15:07:20 +0530

    "closure will return a fucntion"

---
+++ closure.js
@@ -0,0 +1,12 @@
+function makeCounter() {
+  let count = 0; // This variable is in the outer scope
+  return function() {
+    count++;
+    return count;
+  };
+}
+
+const counter = makeCounter();
+console.log(counter()); // 1
+console.log(counter()); // 2
+console.log(counter()); // 3
